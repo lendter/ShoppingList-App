39b03dc7aac603c81467e25e4ad140562dcd85cf
leonendter <LeoEnd@schueler.bwv-ac.de>
2024-01-07 21:02:50 +0100

    initial commit current status

---
+++ App.js
@@ -0,0 +1,6 @@
+import { StyleSheet} from 'react-native';
+import ConditionalRenderedComponent from './components/ConditionalRenderedComponent';
+
+export default function App() {
+  return <ConditionalRenderedComponent></ConditionalRenderedComponent>
+}
